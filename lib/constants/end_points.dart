3c637e29d1a4a2f17681c4199adfbdbfb1bb696b
noorfatima15 <noorfatima13689@gmail.com>
2023-04-15 19:44:03 +0500

    initial Commit

---
+++ lib/constants/end_points.dart
@@ -0,0 +1,4 @@
+class EndPoints {
+  EndPoints._();
+  static const String apiKey = '9fb53a632d64376c3784ba84b3d6f12f';
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/app_view.dart';
+
+void main() {
+  runApp(const AppView());
+}
